c321f1e02999360a4c59affb7d2aa3429cbbb6a7
Anatoly Scherbakov <altaisoft@gmail.com>
2023-10-05 12:57:55 +0400

    `j stack push` now shows only my own PRs by default

--- jeeves_pr_stack/logic.py
+++ jeeves_pr_stack/logic.py
@@ -1,22 +1,78 @@
 import json
+import os
 import sys
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from functools import cached_property
 from typing import Iterable
 
+import funcy
 import sh
 
 from jeeves_pr_stack import github
 from jeeves_pr_stack.errors import MergeConflicts
-from jeeves_pr_stack.models import Commit, PullRequest
+from jeeves_pr_stack.models import Commit, PullRequest, RawPullRequest
 
 
 @dataclass
 class JeevesPullRequestStack:
     """Jeeves PR Stack application."""
 
-    gh: sh.Command
-    git: sh.Command
+    gh: sh.Command = field(default_factory=lambda: sh.gh.bake(
+        _long_sep=None,
+        _env={
+            **os.environ,
+            'NO_COLOR': '1',
+        },
+    ))
+    git: sh.Command = field(default_factory=lambda: sh.git)
+
+    def list_pull_requests(self, author: str | None = None) -> list[PullRequest]:
+        """
+        Retrieve a list of all open PRs in the repo.
+
+        Mark the one bound to current branch with `is_current` field.
+        """
+        fields = [
+            'number',
+            'baseRefName',
+            'headRefName',
+            'id',
+            'isDraft',
+            'mergeable',
+            'title',
+            'url',
+            'reviewDecision',
+            'reviewRequests',
+            'statusCheckRollup',
+        ]
+
+        gh_pr_list = self.gh.pr.list.bake(json=','.join(fields))
+        if author:
+            gh_pr_list = gh_pr_list.bake(author=author)
+
+        raw_pull_requests: list[RawPullRequest] = json.loads(
+            gh_pr_list()
+        )
+
+        return [
+            PullRequest(
+                is_current=(
+                    raw_pull_request['headRefName'] == self.starting_branch
+                ),
+                number=raw_pull_request['number'],
+                base_branch=raw_pull_request['baseRefName'],
+                branch=raw_pull_request['headRefName'],
+                title=raw_pull_request['title'],
+                url=raw_pull_request['url'],
+                is_draft=raw_pull_request['isDraft'],
+                mergeable=raw_pull_request['mergeable'],
+                review_decision=raw_pull_request['reviewDecision'],
+                reviewers=funcy.pluck('login',
+                                      raw_pull_request['reviewRequests']),
+                checks_status=github.construct_checks_status(raw_pull_request),
+            )
+            for raw_pull_request in raw_pull_requests
+        ]
 
     def list_commits(self) -> list[Commit]:
         """List commits for current PR."""
@@ -57,7 +113,7 @@ class JeevesPullRequestStack:
 
     def rebase(self) -> Iterable[PullRequest]:
         """Rebase all PRs in current stack."""
-        stack = github.retrieve_stack(self.starting_branch)
+        stack = self.list_stack()
 
         for pr in stack:
             yield pr
@@ -78,3 +134,9 @@ class JeevesPullRequestStack:
             self.git.push('--force')
 
         self.git.switch(self.starting_branch)
+
+    def list_stack(self) -> list[PullRequest]:
+        return github.construct_stack_for_branch(
+            branch=self.starting_branch,
+            pull_requests=self.list_pull_requests(),
+        )
